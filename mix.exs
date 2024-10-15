c01fef03fb9d1d78653a277e2c2850c6124b2868
micartey <me@micartey.dev>
2024-10-15 21:34:16 +0200

    chore: chunked update

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ClusterBot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
 
       # Docs
@@ -19,20 +21,29 @@ defmodule ClusterBot.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:cachex, "~> 4.0"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Automatically keep track of connected nodes to store and reconnect with them"
+  end
+
+  defp package() do
+    [
+      name: "cluster_bot",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/micartey/cluster-bot"}
     ]
   end
 end
